08401ce0d8e1d94f31c72139a28b23097a8f538a
Morten Eshoej <eshoej@pop-os.lan>
2024-09-21 12:17:02 +0200

    added a bunch of configs

--- configs/neovim/nvim/after/plugin/lsp.lua
+++ configs/neovim/nvim/after/plugin/lsp.lua
@@ -28,7 +28,7 @@ require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
-  ensure_installed = {'lua_ls', 'ts_ls', 'gopls'},
+  ensure_installed = {'lua_ls', 'ts_ls', 'gopls', 'marksman'},
   handlers = {
     function(server_name)
       require('lspconfig')[server_name].setup({})
