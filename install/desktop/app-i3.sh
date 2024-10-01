406de2ab60919a726af25d21c11ceb1af88788a6
Morten Due Eshoej <eshoej@pop-os.lan>
2024-10-01 20:32:44 +0200

    modified

--- install/desktop/app-i3.sh
+++ install/desktop/app-i3.sh
@@ -3,17 +3,17 @@ sudo apt install i3 i3blocks picom feh compton rofi dmenu
 
 if [ ! -d "$HOME/.config/i3" ]; then
     mkdir -p ~/.config/i3
-    cp -r  ~/.local/share/poposetup/configs/i3/i3 ~/.config
+    cp -r  ~/.local/share/poposetup/configs/i3/.config/i3 ~/.config
 fi
 
 
 if [ ! -d "$HOME/.config/i3blocks" ]; then
     mkdir -p ~/.config/i3blocks
-    cp -r  ~/.local/share/poposetup/configs/i3blocks/i3blocks ~/.config
+    cp -r  ~/.local/share/poposetup/configs/i3blocks/.config/i3blocks ~/.config
 fi
 
 if [ ! -d "$HOME/.config/picom" ]; then
     mkdir -p ~/.config/picom
-    cp -r  ~/.local/share/poposetup/configs/picom/picom ~/.config
+    cp -r  ~/.local/share/poposetup/configs/picom/.config/picom ~/.config
 fi
 
