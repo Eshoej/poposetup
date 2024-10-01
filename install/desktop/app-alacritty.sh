171ac6a345bff3d5550552440eed0fbe49db34f6
Morten Due Eshoej <eshoej@pop-os.lan>
2024-10-01 09:38:24 +0200

    setup

--- install/desktop/app-alacritty.sh
+++ install/desktop/app-alacritty.sh
@@ -1,3 +1,3 @@
 # Alacritty is a GPU-powered and highly extensible terminal. See https://alacritty.org/
-sudo cargo installalacritty
+sudo cargo install alacritty
 cp -r  ~/.local/share/poposetup/configs/alacritty ~/.config
